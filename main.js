d5cee111f90f952559fb85a0a1ba07011b27399c
Kristina Todd <Kristina.Todd22@gmail.com>
2020-01-29 17:00:58 -0700

    final push

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 
-
-let userChoice
 let choice = ["rock", "paper", "scissors"]
-let computerChoiceElem = document.getElementById("computer-choice")
 let computerChoice = 0;
 
+function startGame() {
+  setTimeout(function () {
+    document.getElementById("computerChoiceImage").classList.replace("rock", "computer")
+    document.getElementById("computerChoiceImage").classList.replace("scissors", "computer")
+    document.getElementById("computerChoiceImage").classList.replace("paper", "computer")
+    document.getElementById("winning").classList.add("invisible")
+    document.getElementById("losing").classList.add("invisible")
+    document.getElementById("tie").classList.add("invisible")
+  }, 2000)
+}
 
 function drawComputerChoice() {
   computerChoice = Math.floor(Math.random() * choice.length)
@@ -19,6 +26,7 @@ function drawComputerChoice() {
   else {
     document.getElementById("computerChoiceImage").classList.replace("computer", "scissors")
   }
+
 }
 
 
@@ -27,12 +35,17 @@ function playRock() {
   drawComputerChoice()
 
   if (computerChoice == 0) {
+    document.getElementById("tie").classList.remove("invisible")
     console.log("It's a tie!")
   } else if (computerChoice == 1) {
+    document.getElementById("losing").classList.remove("invisible")
     console.log("You lose!")
   } else {
+    document.getElementById("winning").classList.remove("invisible")
     console.log("You win!")
   }
+
+  startGame()
 }
 
 function playScissors() {
@@ -40,12 +53,17 @@ function playScissors() {
   drawComputerChoice()
 
   if (computerChoice == 0) {
+    document.getElementById("losing").classList.remove("invisible")
     console.log("You lose!")
   } else if (computerChoice == 1) {
+    document.getElementById("winning").classList.remove("invisible")
     console.log("You win!")
   } else {
+    document.getElementById("tie").classList.remove("invisible")
     console.log("It's a tie!")
   }
+
+  startGame()
 }
 
 function playPaper() {
@@ -53,11 +71,16 @@ function playPaper() {
   drawComputerChoice()
 
   if (computerChoice == 0) {
+    document.getElementById("winning").classList.remove("invisible")
     console.log("You win!")
   } else if (computerChoice == 1) {
+    document.getElementById("tie").classList.remove("invisible")
     console.log("It's a tie!")
   } else {
+    document.getElementById("losing").classList.remove("invisible")
     console.log("You Lose!")
   }
+
+  startGame()
 }
 
